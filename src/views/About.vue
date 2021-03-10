0ea2b08be7e6d7b40e15fdd4eaaf8454c1d64738
JC <yo@com.com>
2021-03-09 22:31:36 -0600

    updates to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class=".dflex justify-center align-center">
+    <v-card elevation="5" class="ma-5">
+      <v-card-title>
+        <h1>Hola!</h1>
+      </v-card-title>
+      <v-card-text>
+        <p>
+          Este es un proyecto sencillo y colaborativo por un grupo de tres
+          aspirantes a ingenieros. Este proyecto no representa una empresa real,
+          ni mucho menos una seria. Por favor apreciar esta pagina por su
+          estetica y no por su calidad de contenido.
+        </p>
+      </v-card-text>
+    </v-card>
+    <v-card elevation="5" class="ma-5">
+      <v-card-title>
+        Integrantes
+      </v-card-title>
+      <v-timeline align-top dense>
+        <v-timeline-item> </v-timeline-item>
+      </v-timeline>
+    </v-card>
+  </v-container>
 </template>
