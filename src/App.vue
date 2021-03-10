657e67336caea03f8ba79ce152ddfb6b0c9b539f
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-03-09 21:39:13 -0600

    Añadido router view a app.vue

--- src/App.vue
+++ src/App.vue
@@ -48,23 +48,23 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+//    HelloWorld,
   },
 
   data: () => ({
-    //
+    
   }),
-};
+}; 
 </script>
