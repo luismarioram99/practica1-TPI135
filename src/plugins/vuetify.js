a5894cb7e451782f0a0cde60624d27dcb7e23926
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-03-09 21:27:02 -0600

    Añadida navbar, falta verificar routing

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#0A2463',
+        secondary: '#FB3640',
+        accent: '#FB3640',        
+      }
+    }
+  }
 });
