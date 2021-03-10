5fdb7a6c987b76df0a6e133df4b43487fc46b1b8
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-03-09 22:50:34 -0600

    Añadida info a landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,14 +26,13 @@
       <v-row>
         <v-col cols="12" md="6" lg="4">
           <v-card class="mx-auto" max-width="344">
-            <v-img
-              src="../assets/hospital1.jpg"
-              height="200px"
-            ></v-img>
+            <v-img src="../assets/hospital1.jpg" height="200px"></v-img>
 
             <v-card-title> La mejor atención para ti</v-card-title>
 
-            <v-card-subtitle> Instalaciones de primera calidad para tu familia </v-card-subtitle>
+            <v-card-subtitle>
+              Instalaciones de primera calidad para tu familia
+            </v-card-subtitle>
 
             <v-card-actions>
               <v-btn color="orange lighten-2" text> Mostrar más </v-btn>
@@ -52,22 +51,27 @@
                 <v-divider></v-divider>
 
                 <v-card-text>
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam tristique dignissim magna in scelerisque. Aliquam non justo ut augue scelerisque vestibulum. Sed ac luctus lacus. Suspendisse sollicitudin tortor quis laoreet sodales. Proin laoreet tortor vitae ante sagittis, ut pellentesque libero fringilla. Curabitur ac est arcu.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Aliquam tristique dignissim
+                  magna in scelerisque. Aliquam non justo ut augue scelerisque
+                  vestibulum. Sed ac luctus lacus. Suspendisse sollicitudin
+                  tortor quis laoreet sodales. Proin laoreet tortor vitae ante
+                  sagittis, ut pellentesque libero fringilla. Curabitur ac est
+                  arcu.
                 </v-card-text>
               </div>
             </v-expand-transition>
           </v-card>
-          </v-col>
+        </v-col>
         <v-col cols="12" md="6" lg="4">
           <v-card class="mx-auto" max-width="344">
-            <v-img
-              src="../assets/hospital2.jpg"
-              height="200px"
-            ></v-img>
+            <v-img src="../assets/hospital2.jpg" height="200px"></v-img>
 
             <v-card-title> Personal de la mejor calidad! </v-card-title>
 
-            <v-card-subtitle> Deja tu salud en nuestras manos. </v-card-subtitle>
+            <v-card-subtitle>
+              Deja tu salud en nuestras manos.
+            </v-card-subtitle>
 
             <v-card-actions>
               <v-btn color="orange lighten-2" text> Mostrar más </v-btn>
@@ -86,22 +90,26 @@
                 <v-divider></v-divider>
 
                 <v-card-text>
-                  Vestibulum maximus placerat ultricies. Pellentesque ipsum est, viverra eu auctor id, sollicitudin condimentum mi. Aenean porttitor ac lacus in tristique. Curabitur maximus metus in nisl blandit gravida. Proin convallis libero eget accumsan auctor. Duis id massa in lectus pellentesque porta. Ut dictum tortor imperdiet massa pellentesque convallis.
+                  Vestibulum maximus placerat ultricies. Pellentesque ipsum est,
+                  viverra eu auctor id, sollicitudin condimentum mi. Aenean
+                  porttitor ac lacus in tristique. Curabitur maximus metus in
+                  nisl blandit gravida. Proin convallis libero eget accumsan
+                  auctor. Duis id massa in lectus pellentesque porta. Ut dictum
+                  tortor imperdiet massa pellentesque convallis.
                 </v-card-text>
               </div>
             </v-expand-transition>
           </v-card>
-          </v-col>
+        </v-col>
         <v-col cols="12" md="6" lg="4">
           <v-card class="mx-auto" max-width="344">
-            <v-img
-              src="../assets/hospital3.jpg"
-              height="200px"
-            ></v-img>
+            <v-img src="../assets/hospital3.jpg" height="200px"></v-img>
 
             <v-card-title> Tenemos la vacuna del Covid-19 </v-card-title>
 
-            <v-card-subtitle> Directita de la mano de Nayib Bukele </v-card-subtitle>
+            <v-card-subtitle>
+              Directita de la mano de Nayib Bukele
+            </v-card-subtitle>
 
             <v-card-actions>
               <v-btn color="orange lighten-2" text> Mostrar más </v-btn>
@@ -110,7 +118,7 @@
 
               <v-btn icon @click="show3 = !show3">
                 <v-icon>{{
-                  show3? "mdi-chevron-up" : "mdi-chevron-down"
+                  show3 ? "mdi-chevron-up" : "mdi-chevron-down"
                 }}</v-icon>
               </v-btn>
             </v-card-actions>
@@ -120,31 +128,74 @@
                 <v-divider></v-divider>
 
                 <v-card-text>
-                 Maecenas ultricies risus elit, vel lobortis dui viverra a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras nec ultricies nulla. Morbi quis congue velit. Suspendisse ac dolor a neque fringilla malesuada. Nulla viverra vitae metus laoreet rhoncus. 
+                  Maecenas ultricies risus elit, vel lobortis dui viverra a.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Cras nec ultricies nulla.
+                  Morbi quis congue velit. Suspendisse ac dolor a neque
+                  fringilla malesuada. Nulla viverra vitae metus laoreet
+                  rhoncus.
                 </v-card-text>
               </div>
             </v-expand-transition>
           </v-card>
         </v-col>
       </v-row>
+      <v-divider class="my-3"></v-divider>
+
+      <v-sparkline
+        :value="value"
+        :gradient="gradient"
+        :smooth="radius || false"
+        :padding="padding"
+        :line-width="width"
+        :stroke-linecap="lineCap"
+        :gradient-direction="gradientDirection"
+        :fill="fill"
+        :type="type"
+        :auto-line-width="autoLineWidth"
+        auto-draw
+      ></v-sparkline>
     </v-container>
+    <center>
+      <h3>
+        Llevamos registro de tu salud con
+        <span class="red--text">tu ID de paciente.</span>
+      </h3>
+    </center>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+const gradients = [
+  ["#222"],
+  ["#42b3f4"],
+  ["red", "orange", "yellow"],
+  ["purple", "violet"],
+  ["#00c6ff", "#F0F", "#FF0"],
+  ["#f72047", "#ffd200", "#1feaea"],
+];
 export default {
   name: "Home",
-  components: {
-  },
-  data (){
+  components: {},
+  data() {
     return {
       show1: false,
       show2: false,
       show3: false,
-    }
-  }
+      width: 2,
+      radius: 10,
+      padding: 8,
+      lineCap: "round",
+      gradient: gradients[5],
+      value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+      gradientDirection: "top",
+      gradients,
+      fill: false,
+      type: "trend",
+      autoLineWidth: false,
+    };
+  },
 };
 </script>
 
